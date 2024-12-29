3589f1bc17a92298cfe7d250003726ffa6d121fb
Israel Teixeira de Andrade <israeltandrade@hotmail.com>
2024-12-29 08:18:54 -0300

    Basic data update

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,7 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
@@ -16,57 +13,83 @@ public class Program {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-
-//------INSERT DATA:
-
 		PreparedStatement st = null;
+
+//------UPDATE DATA:
+		
 		try {
 			conn = DB.getConnection();
-
-			st = conn.prepareStatement(
-					"INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES "
-							+ "(?, ?, ?, ?, ?)",
-					// Optional return of id:
-					Statement.RETURN_GENERATED_KEYS);
 			
-//			Replace the interrogations ("?"s) on the previous PreparedStatement object:
-			st.setString(1, "Carl Purple");
-			st.setString(2, "carl@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-			st.setDouble(4, 3000.0);
-			st.setInt(5, 4);
-
-			// Second example with simple insertion (comment ParseException to work):
-//			st = conn.prepareStatement(
-//					"insert into department (Name) values ('D1'),('D2')",
-//					Statement.RETURN_GENERATED_KEYS);
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+//			First parameter refers to the "?", the second one is the value:
+			st.setDouble(1,  200.0);
+			st.setInt(2, 2);
 			
 			int rowsAffected = st.executeUpdate();
-
-//			Old simple msg return:
-//			System.out.println("Done! Rows affected: " + rowsAffected);
-
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Done! Id = " + id);
-				}
-			} else {
-				System.out.println("No rows affected!");
-			}
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		catch (ParseException e) {
-			e.printStackTrace();
-		}
 		finally {
 			DB.closeStatement(st);
-			
 			DB.closeConnection();
 		}
+		
+//------INSERT DATA:
+
+//		try {
+//			conn = DB.getConnection();
+//
+//			st = conn.prepareStatement(
+//					"INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary, DepartmentId) " + "VALUES "
+//							+ "(?, ?, ?, ?, ?)",
+//					// Optional return of id:
+//					Statement.RETURN_GENERATED_KEYS);
+//			
+////			Replace the interrogations ("?"s) on the previous PreparedStatement object:
+//			st.setString(1, "Carl Purple");
+//			st.setString(2, "carl@gmail.com");
+//			st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+//			st.setDouble(4, 3000.0);
+//			st.setInt(5, 4);
+//
+//			// Second example with simple insertion (comment ParseException to work):
+////			st = conn.prepareStatement(
+////					"insert into department (Name) values ('D1'),('D2')",
+////					Statement.RETURN_GENERATED_KEYS);
+//			
+//			int rowsAffected = st.executeUpdate();
+//
+////			Old simple msg return:
+////			System.out.println("Done! Rows affected: " + rowsAffected);
+//
+//			if (rowsAffected > 0) {
+//				ResultSet rs = st.getGeneratedKeys();
+//				while (rs.next()) {
+//					int id = rs.getInt(1);
+//					System.out.println("Done! Id = " + id);
+//				}
+//			} else {
+//				System.out.println("No rows affected!");
+//			}
+//		}
+//		catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		catch (ParseException e) {
+//			e.printStackTrace();
+//		}
+//		finally {
+//			DB.closeStatement(st);
+//			
+//			DB.closeConnection();
+//		}
 
 //------RETRIEVE DATA:
 //
