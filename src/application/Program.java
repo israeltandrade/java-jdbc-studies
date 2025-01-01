630fefc5be5aee11ccb07303bdf970c54240ce83
Israel Teixeira de Andrade <israeltandrade@hotmail.com>
2025-01-01 14:25:44 -0300

    Simple demo transactions

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 public class Program {
 
@@ -14,35 +14,71 @@ public class Program {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 
-		//------DELETE DATA:
-		
-				try {
-					conn = DB.getConnection();
-					
-					st = conn.prepareStatement(
-							"DELETE FROM department "
-							+ "WHERE "
-							+ "Id = ?");
-					
-					//	First parameter refers to the "?", the second one is the value:
-					st.setInt(1, 2);
-					
-					int rowsAffected = st.executeUpdate();
-					
-					System.out.println("Done! Rows affected: " + rowsAffected);
-				}
-				catch (SQLException e) {
-					throw new DbIntegrityException(e.getMessage());
-				}
-				finally {
-					DB.closeStatement(st);
-					DB.closeConnection();
-				}		
+		try {
+			conn = DB.getConnection();
+
+			conn.setAutoCommit(false);
+			
+			st = conn.createStatement();
+
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+//			int x = 1;
+//			if (x < 2) {
+//				throw new SQLException("Fake error");
+//			}
+			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+			conn.commit();
+			
+			System.out.println("rows1 " + rows1);			
+			System.out.println("rows2 " + rows2);			
+		}
+		catch (SQLException e) {
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+//		PreparedStatement st = null;
+
+		// ------DELETE DATA:
+
+//				try {
+//					conn = DB.getConnection();
+//					
+//					st = conn.prepareStatement(
+//							"DELETE FROM department "
+//							+ "WHERE "
+//							+ "Id = ?");
+//					
+//					//	First parameter refers to the "?", the second one is the value:
+//					st.setInt(1, 2);
+//					
+//					int rowsAffected = st.executeUpdate();
+//					
+//					System.out.println("Done! Rows affected: " + rowsAffected);
+//				}
+//				catch (SQLException e) {
+//					throw new DbIntegrityException(e.getMessage());
+//				}
+//				finally {
+//					DB.closeStatement(st);
+//					DB.closeConnection();
+//				}		
 
 //------UPDATE DATA:
-		
+
 //		try {
 //			conn = DB.getConnection();
 //			
@@ -66,7 +102,7 @@ public class Program {
 //			DB.closeStatement(st);
 //			DB.closeConnection();
 //		}
-		
+
 //------INSERT DATA:
 
 //		try {
